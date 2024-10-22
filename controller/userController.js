91364e74c8c48bed4e5bce246c625a4d6ba41257
shanavas <118879706+ShanavasRahman@users.noreply.github.com>
2024-10-22 13:17:28 +0530

    model and controller setup

---
+++ controller/userController.js
@@ -0,0 +1,20 @@
+const User = require('../model/userModel');
+
+const createUser = async (req, res) => {
+    try {
+        const { name, email, address } = req.body;
+        const newUser = new User({
+            name,
+            email,
+            address
+        })
+        const userExist = await User.findOne({ email });
+        if (userExist) {
+            return res.status(400).json({ message: "user already exist" });
+        }
+        const savedData = await newUser.save();
+        res.status(200).json(savedData);
+    } catch (error) {
+        res.status(500).json({errorMessage:error.message})
+    }
+}
