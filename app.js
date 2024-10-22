86ebf527c7de14b5e8a57b8277e19a711fc398f1
shanavas <118879706+ShanavasRahman@users.noreply.github.com>
2024-10-22 12:48:01 +0530

    server created

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+app.use(express.json());
+
+dotenv.config();
+
+const PORT = process.env.PORT || 7000;
+const MONGO_URL = process.env.MONGO_URL;
+app.get('/', (req, res) => {
+    res.send("Hello world");
+})
+
+mongoose
+    .connect(MONGO_URL)
+    .then(() => {
+        console.log("DB connected successfully");
+
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.error("DB connection failed:", error);
+    });
